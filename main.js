a3f2faea9d19efa63ddc786414eaceee3ed1c0e8
Abhishek Baghel <142092745+abhikiing@users.noreply.github.com>
2024-02-14 23:31:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,30 @@ window.onscroll = function(){
     btnMenu.classList.remove('fa-times')
     Menu.classList.remove('active')
 }
+
+/* email.js */
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      mobile: document.getElementById("mobile").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_gyqgu69";
+    const templateID = "template_2iub77g";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("mobile").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully to Abhishek")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+            
